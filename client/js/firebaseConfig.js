45082242c3a9169eb66ba7e9f4a7576b4082f2ce
rdhforeclosureconqueror-sys <rdhforeclosureconqueror@gmail.com>
2025-12-12 02:31:53 -0600

    Create firebaseConfig.js

---
+++ client/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// firebaseConfig.js
+// paste your own credentials from Firebase console
+const firebaseConfig = {
+  apiKey: "AIzaSyCtG2ICZpJwu0S5_0ybyQUTAQkIvkxDM0k",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "fitnessmvp-9923c",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+export default firebaseConfig;
